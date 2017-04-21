51eda7e8e6085ca15bcc1083d4faaa776075751c
Xander <xackhackman@gmail.com>
2017-04-21 12:54:49 -0600

    3

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,32 +1,24 @@
+/* global React ReactDOM */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MyTitle from './MyTitle'
+
 var div = React.DOM.div
-var h1 = React.DOM.h1
 
-
-var myTitle = React.createClass({
-	render: function() {
-		return (
-			div(null,
-				h1({style:{color:this.props.color}},this.props.title)
-			)
-		)
-	}
-})
-
-var MyTitleFactory = React.createFactory(myTitle)
-
+var MyTitleFactory = React.createFactory(MyTitle)
 
 var myFirstComponent = React.createClass({
-	render: function() {
-		return (
+  render: function () {
+    return (
 			div(null,
-				MyTitleFactory({title: 'props are the best', color:'peru'}),
-				MyTitleFactory({title: 'Semicolons are the worst', color:'mediumaquamarine'}),
-				MyTitleFactory({title: 'wassup', color:'rebeccapurple'}),
-				MyTitleFactory({title: 'The final countdown', color:'darkviletred'})
+				MyTitleFactory({title: 'props are the best', color: 'peru'}),
+				MyTitleFactory({title: 'Semicolons are the worst', color: 'mediumaquamarine'}),
+				MyTitleFactory({title: 'wassup', color: 'rebeccapurple'}),
+				MyTitleFactory({title: 'The final countdown', color: 'dodgerblue'})
 			)
-		)
-	}
+    )
+  }
 })
 
-
-ReactDOM.render(React.createElement(myFirstComponent), document.getElementById('app'));
+ReactDOM.render(React.createElement(myFirstComponent), document.getElementById('app'))
