a36d0584b20be242364df4f9c62e12e4fc91cb8e
Xander <xackhackman@gmail.com>
2017-04-26 15:47:01 -0600

    basic unit tests with jest/enzyme

---
+++ js/Search.spec.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Search from './Search'
+import ShowCard from './ShowCard'
+import preload from '../public/data.json'
+import { shallow } from 'enzyme'
+import { shallowToJson } from 'enzyme-to-json'
+
+test('Search snapshot test', () => {
+	const component = shallow(<Search />)
+	const tree = shallowToJson(component)
+	expect(tree).toMatchSnapshot()
+})
+
+test('Search should render a ShowCard for each show', () => {
+	const component = shallow(<Search />)
+	expect(component.find(ShowCard).length).toEqual(preload.shows.length)
+})
+
+test("Search should render correct amount of shows based on search", () => {
+	const searchWord = 'house'
+	const component = shallow(<Search />)
+	component.find('input').simulate('change', {target: {value: searchWord}})
+	const showCount = preload.shows.filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(searchWord.toUpperCase()) >= 0).length
+	expect(component.find(ShowCard).length).toEqual(showCount)
+})
