ce9ce83ac0b48f70590d75dcc108da874748c4da
Xander <xackhackman@gmail.com>
2017-04-29 00:03:54 -0600

    Adding Axios Ajax request to OMDBapi

---
+++ js/Header.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+class Header extends React.Component {
+
+  render () {
+    let utilSpace
+    if (this.props.showSearch) {
+      utilSpace = <input onChange={this.props.handleSearchTermChange} value={this.props.searchTerm} type='text' placeholder='Search' />
+    } else {
+      utilSpace = (
+        <h2>
+          <Link to='/search'>
+            Back
+          </Link>
+        </h2>
+      )
+    }
+    return (
+      <header>
+        <h1>
+          <Link to='/'>
+            svideo
+          </Link>
+        </h1>
+        {utilSpace}
+      </header>
+    )
+  }
+}
+
+const {func, bool, string } = React.PropTypes
+Header.propTypes = {
+  handleSearchTermChange:func,
+  showSearch: bool,
+  searchTerm: string
+}
+
+export default Header
--- js/ShowCard.js
+++ js/ShowCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router'
 const { string } = React.PropTypes
 
 const ShowCard = React.createClass({
@@ -6,19 +7,22 @@ const ShowCard = React.createClass({
     poster: string,
     title: string,
     year: string,
-    description: string
+    description: string,
+    imdbID: string
   },
   render () {
-    const { poster, title, year, description } = this.props
+    const { poster, title, year, description, imdbID } = this.props
     return (
-      <div className='show-card'>
-        <img src={`/public/img/posters/${poster}`} />
-        <div>
-          <h3>{title}</h3>
-          <h4>({year})</h4>
-          <p>{description}</p>
+      <Link to={`/details/${imdbID}`}>
+        <div className='show-card'>
+          <img src={`/public/img/posters/${poster}`} />
+          <div>
+            <h3>{title}</h3>
+            <h4>({year})</h4>
+            <p>{description}</p>
+          </div>
         </div>
-      </div>
+      </Link>
     )
   }
 })
